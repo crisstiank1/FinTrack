9df5a0979f44cc44699cb5e739f8194f8b546d19
crisstiank1 <cristianpescoi15@gmail.com>
2026-09-06 15:17:53 -0500

    feat: add finanzas deps (dinero.js, papaparse), supabase CLI, shadcn sonner, path aliases, fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,9 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'typescript-eslint'
 
 export default tseslint.config(
-  globalIgnores(['dist']),
+  { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
